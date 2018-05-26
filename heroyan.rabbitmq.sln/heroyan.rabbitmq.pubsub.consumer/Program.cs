c97867653c3968d83ba871e053ca5da2d09e57d2
HeroYan <386799757@qq.com>
2018-05-27 00:06:48 +0800

    rabbitmq pubsub demo
    rabbitmq pubsub demo

---
+++ heroyan.rabbitmq.sln/heroyan.rabbitmq.pubsub.consumer/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace heroyan.rabbitmq.pubsub.consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory
+            {
+                HostName = "localhost",
+                Port = 5672
+            };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "logs", type: "fanout");
+
+                var queueName = channel.QueueDeclare().QueueName;
+                channel.QueueBind(queue: queueName, exchange: "logs", routingKey: "");
+
+                Console.WriteLine("[*] Waiting for logs.");
+
+                var consumer = new EventingBasicConsumer(channel);
+
+                consumer.Received += (obj, e) =>
+                {
+                    var body = e.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine("[x] Received {0}", message);
+                };
+
+                channel.BasicConsume(queue: queueName, autoAck: true, consumer: consumer);
+
+                Console.WriteLine("Press [enter] to exit.");
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ heroyan.rabbitmq.sln/heroyan.rabbitmq.pubsub.producer/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace heroyan.rabbitmq.pubsub.producer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory
+            {
+                HostName = "localhost",
+                Port = 5672
+            };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare(queue: "task_queue", durable: true, exclusive: false, autoDelete: false, arguments: null); // 队列持久化                
+
+                var properties = channel.CreateBasicProperties();
+                properties.Persistent = true; // 消息持久化
+
+                var msg = "hello";
+
+                for (int i = 0; i < 20; i++)
+                {
+                    msg += ".";
+
+                    var body = Encoding.UTF8.GetBytes(msg);
+
+                    channel.BasicPublish(exchange: "", routingKey: "task_queue", basicProperties: properties, body: body);
+
+                    Console.WriteLine($"[x] Sent {msg}");
+                }
+            }
+
+            Console.WriteLine("Press [enter] to exit.");
+            Console.ReadKey();
+        }
+    }
+}
